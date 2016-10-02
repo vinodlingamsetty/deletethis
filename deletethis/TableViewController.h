c12501cde455e9c971883726010090d16fdee02f
vinodlingamsetty <vinodlingamsetty@gmail.com>
2016-10-01 23:06:42 -0400

    abc

---
+++ deletethis/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  deletethis
+//
+//  Created by vinod kumar lingamsetty on 9/29/16.
+//  Copyright © 2016 vinod kumar lingamsetty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
--- deletethis/ViewController.m
+++ deletethis/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface ViewController ()
-
+@interface ViewController (){
+    //MPMoviePlayerController *_player;
+}
 @end
 
 @implementation ViewController
@@ -17,6 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSURL *urlString = @"";
+    
 }
 
 
@@ -25,5 +29,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+//- (IBAction)playBtn:(id)sender {
+//    
+//    NSURL *streamingURL = [NSURL URLWithString:@"http://cdn3.viblast.com/streams/hls/airshow/playlist.m3u8"];
+//    _player = [[MPMoviePlayerController alloc]initWithContentURL:streamingURL];
+//    _player.view.frame = CGRectMake(0, 0, self.view.frame.size.width, 300);
+//    [self.view addSubview:_player.view];
+//    [_player play];
+//}
 
 @end
